3c921a3304beff2dedf37d8d6c5ba74739d3c5d6
Mateo <mateocarballo2@gmail.com>
2024-02-02 12:31:51 +0100

    Patron singleton en la conexion a DB

--- untitled/src/main/java/org/example/ConexionDB/Conexion.java
+++ untitled/src/main/java/org/example/ConexionDB/Conexion.java
@@ -4,20 +4,29 @@ import java.sql.*;
 import java.util.ArrayList;
 
 public class Conexion {
-    String url;
-    String user;
-    String password;
-    Connection conexion;
-    Statement statement;
-    ResultSet resultSet;
 
-    public Conexion(String url, String user, String password){
+    private static Conexion singleton;
+    private String url;
+    private String user;
+    private String password;
+    private Connection conexion;
+    private Statement statement;
+    private ResultSet resultSet;
+
+    private Conexion(String url, String user, String password){
         this.url = url;
         this.user = user;
         this.password=password;
         this.conexion= null;
         this.statement=null;
         this.resultSet=null;
+    }
+
+    public static Conexion singleton (String url, String user, String password){
+        if(singleton == null){
+            singleton = new Conexion(url, user, password);
+        }
+        return singleton;
     }
 
 
